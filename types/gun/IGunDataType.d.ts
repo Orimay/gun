c45d7a5d14723bf755a8e35281066b655174b971
dbaranov <dbaranov@bellintegrator.com>
2022-01-24 00:18:43 +0300

    Temporarily removing types

--- types/gun/IGunDataType.d.ts
+++
@@ -1,8 +0,0 @@
-export type IGunDataType = {
-    [P in string]: IGunDataType | string | number | boolean | null | undefined
-}&
-{
-    [T in number]: IGunDataType | string | number | boolean | null | undefined
-}
-
-export type IGunNodeDataType = IGunDataType | string | number | boolean | null | undefined
